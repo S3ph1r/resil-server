c56b1739d73957d771ba28de3341b04cc9372b14
S3ph1r <guareschi.roberto@gmail.com>
2025-07-22 01:39:34 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,5 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:2.3.9")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.9")
     implementation("io.ktor:ktor-serialization-gson-jvm:2.3.9")
-    implementation("io.ktor:ktor-server-call-logging-jvm:2.3.9")
     implementation("ch.qos.logback:logback-classic:1.4.14")
 }
