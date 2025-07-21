523cea803a24c4d5766757fe8eb690b12b768b76
S3ph1r <guareschi.roberto@gmail.com>
2025-07-22 00:50:57 +0200

    Update Application.kt

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,17 @@
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    // Avvia il server Netty sulla porta 8080, accessibile da ovunque ("0.0.0.0")
+    // Render userà la porta 10000, ma Ktor si adatta automaticamente.
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 }
 
+// Il modulo principale dell'applicazione, chiamato quando il server parte.
 fun Application.module() {
-    configureMonitoring()
+    // Chiama le funzioni di configurazione che abbiamo definito negli altri file.
     configureSerialization()
     configureRouting()
 }
